9a25f1bebe890c5c08a85007c006630f6f8f2742
Kamil Belan <belankkamil@gmail.com>
2024-04-18 20:04:00 +0200

    fixed an missing end

--- src/grids.jl
+++ src/grids.jl
@@ -81,8 +81,8 @@ end
 
 function covering(grid::Expgrid, s::Shape)::Vector{RealVector}
     N = grid.N
-    H = 7.99e3 / 1e4  # scale atmosphere height
-    γ = 7 / 5
+    H = 7.99e3   # scale atmosphere height
+    γ = 7/5
     g = 9.81
     ρ₀ = 1.177
     xs = RealVector[]
@@ -90,9 +90,8 @@ function covering(grid::Expgrid, s::Shape)::Vector{RealVector}
     i_min = Int64(floor(rect.x1_min / grid.dr))
     j_min = Int64(floor(rect.x2_min / grid.dr))
     i_max = Int64(ceil(rect.x1_max / grid.dr))
-    j_max_base = Int64(ceil(rect.x2_max / grid.dr))
-
-    for i in i_min:i_max for j in j_min:j_max
+    j_max = Int64(ceil(rect.x2_max / grid.dr))
+    for i in i_min:i_max , j in j_min:j_max
         x1 = i * grid.dr
         x2 = ρ₀ * exp(-j * grid.dr * (N / g + 1 / (H * γ)))
         x = RealVector(x1, x2, 0.0)
