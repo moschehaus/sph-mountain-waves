ea0036e1f71f5c7f5a215a11f2abf314fdbe2c9c
moschehaus <belankkamil@gmail.com>
2025-11-25 01:47:30 +0100

    new attempt at packing algo

--- examples/utils/atmo_packing.jl
+++ examples/utils/atmo_packing.jl
@@ -1,34 +1,8 @@
 #Implementation of the packing algorithm for the witch simulations from the aritcle: "Particle packing algorithm for SPH schemes, doi: 10.1016/j.cpc.2012.02.032"#
 # 
-module atmo_packing
-
-using SmoothedParticles
 using Statistics
 using LinearAlgebra
 
-
-#geometry parameters
-const dom_height = 26.0    #height of the domain 
-const dr = dom_height/25 #average particle distance (decrease to make finer simulation)
-const h =1.8*dr              
-
-#physical parameters
-const rho0 =1.393		    #referential fluid density
-
-#meteorological parameters
-const g=9.81
-const R_mass=287.05
-const T=250
-const c = sqrt(65e3*(7/5)/rho0)	 
-
-#temporal parameters
-const dt = 0.01*h/c          #time step
-
-#particle types
-const FLUID = 0.0
-const INFLOW = 1.0
-const WALL = 2.0
-const MOUNTAIN = 3.0
 
 """
     packing(system::ParticleSystem,abs_tol::Float64,rel_tol::Float64,maxSteps::Int64)
@@ -36,69 +10,69 @@ For the given system, abs_tol, rel_tol, maxSteps finds a stable initial conditio
 are lesser then a stopping criterion based on abs_tol, rel_tol. If the criterion is not met, ends after maxSteps iterations.
 
 """
-function packing(system::ParticleSystem,abs_tol::Float64,rel_tol::Float64,maxSteps::Int64)
-    apply!(system,reset!) #hard reset of all velocities and accelerations
-    particles=system.particles
-    β,ζ,V0=get_packing_pars(system)
-    apply!(system, (p, q,r) -> find_gGamma!(p, q, r, V0),self=true)
-    apply!(system, p->stabilization_force!(p,β,ζ))
-    
-    init_gGamma=[]
-    for par in particles
-        ∇Γ=par.gGamma
-        push!(init_gGamma,∇Γ)
-    end
-    res_vel=0.0 #velocity residuum can be initialized as zero as it has been reset
-    res_gGamma=LinearAlgebra.norm(init_gGamma)
-    println("-----------------PACKING ALGORITHM INITIALIZED---------------")
-    println("Initial norm of ∇Γ is $res_gGamma")
-    numSteps=0
-    while (((res_vel+res_gGamma)>=stopping_criterion(abs_tol,rel_tol,init_gGamma)) && numSteps<maxSteps)
-        apply!(system, packing_accelerate!)
-        apply!(system, packing_move!)
-        create_cell_list!(system)
-        apply!(system, (p, q,r) -> find_gGamma!(p, q, r, V0),self=true)
-        apply!(system, p->stabilization_force!(p,β,ζ))
-        apply!(system, packing_accelerate!)
+function packing(system::ParticleSystem, abs_tol::Float64, rel_tol::Float64, maxSteps::Int64)
+        apply!(system, reset!) #hard reset of all velocities and accelerations
+        particles = system.particles
+        β, ζ, V0 = get_packing_pars(system)
+        apply!(system, (p, q, r) -> find_gGamma!(p, q, r, V0), self=true)
+        apply!(system, p -> stabilization_force!(p, β, ζ))
 
-        velocities=[]
-        gGamma=[]
+        init_gGamma = []
         for par in particles
-            u=par.u
-            ∇Γ=par.gGamma
-            push!(velocities,u)
-            push!(gGamma,∇Γ)
+                ∇Γ = par.gGamma
+                push!(init_gGamma, ∇Γ)
         end
-        res_vel=LinearAlgebra.norm(velocities)
-        res_gGamma=LinearAlgebra.norm(gGamma)
-        numSteps+=1
-    end
+        res_vel = 0.0 #velocity residuum can be initialized as zero as it has been reset
+        res_gGamma = LinearAlgebra.norm(init_gGamma)
+        println("-----------------PACKING ALGORITHM INITIALIZED---------------")
+        println("Initial norm of ∇Γ is $res_gGamma")
+        numSteps = 0
+        while (((res_vel + res_gGamma) >= stopping_criterion(abs_tol, rel_tol, init_gGamma)) && numSteps < maxSteps)
+                apply!(system, packing_accelerate!)
+                apply!(system, packing_move!)
+                create_cell_list!(system)
+                apply!(system, (p, q, r) -> find_gGamma!(p, q, r, V0), self=true)
+                apply!(system, p -> stabilization_force!(p, β, ζ))
+                apply!(system, packing_accelerate!)
 
-    if numSteps < maxSteps
-        println("Packing successful after $numSteps iterations with velocities norm $res_vel and ∇Γ norm $res_gGamma. Velocities will be set to zero now.")
-    else
-        println("Packing unsuccessful, maximum number of iterations reached, velocities norm $res_vel and ∇Γ norm $res_gGamma. Velocities will be set to zero now")
-    end
-    println("-----------------PACKING ALGORITHM FINISHED---------------")
-    apply!(system,reset!) #hard reset of all velocities and accelerations
-    return system
+                velocities = []
+                gGamma = []
+                for par in particles
+                        v = par.v
+                        ∇Γ = par.gGamma
+                        push!(velocities, v)
+                        push!(gGamma, ∇Γ)
+                end
+                res_vel = LinearAlgebra.norm(velocities)
+                res_gGamma = LinearAlgebra.norm(gGamma)
+                numSteps += 1
+        end
+
+        if numSteps < maxSteps
+                println("Packing successful after $numSteps iterations with velocities norm $res_vel and ∇Γ norm $res_gGamma. Velocities will be set to zero now.")
+        else
+                println("Packing unsuccessful, maximum number of iterations reached, velocities norm $res_vel and ∇Γ norm $res_gGamma. Velocities will be set to zero now")
+        end
+        println("-----------------PACKING ALGORITHM FINISHED---------------")
+        apply!(system, reset!) #hard reset of all velocities and accelerations
+        return system
 end
 
 #=
 Calculate the stabilization force. It consists of a viscous term and a gradient of "uneveness" of distr. of particles
 =#
 
-@inbounds function stabilization_force!(p::AbstractParticle,β::Float64,ζ::Float64)
-    p.Du=-β * p.gGamma- ζ * p.u
+@inbounds function stabilization_force!(p::AbstractParticle, β::Float64, ζ::Float64)
+        p.Dv = -β * p.gGamma - ζ * p.v
 end
 
 #=
 Calculate the graident of gamma, a measure of "uneveness" od particle distribution
 =#
 
-@inbounds function find_gGamma!(p::AbstractParticle, q::AbstractParticle,r::Float64,V0::Float64)
-    x_pq=p.x-q.x
-    p.gGamma += V0*rDwendland2(h,r)*x_pq    
+@inbounds function find_gGamma!(p::AbstractParticle, q::AbstractParticle, r::Float64, V0::Float64)
+        x_pq = p.x - q.x
+	p.gGamma += V0 * rDwendland2(p.h, r) * x_pq
 end
 
 #=
@@ -106,27 +80,27 @@ Get the parameters for the stabilization force: average density, volume and pres
 =#
 
 function get_packing_pars(system::ParticleSystem)
-    
-    particles=system.particles
-    
-    K = g / (T * R_mass)
-    ymin=0.0
-    ymax=dom_height
-    p0=(rho0^2 * T^2 * R_mass^2 / g) * (exp(-K * ymin) - exp(-K * ymax)) #average pressure
-    ρ0=(rho0 * T * R_mass / g) * (exp(-K * ymin) - exp(-K * ymax)) #average density
-    
-    volume=[]
-    for par in particles
-        vol=par.m/par.rho
-        push!(volume,vol)
-    end
-    V0=Statistics.mean(volume) #average volume
-    
-    α=5e-3 #viscosity parameter
-    β=2*p0/ρ0
-    ζ=α*sqrt(β/V0)
 
-    return β, ζ, V0
+        particles = system.particles
+
+        K = g / (T0 * R_mass)
+        ymin = 0.0
+        ymax = dom_height
+        p0 = (rho0^2 * T0^2 * R_mass^2 / g) * (exp(-K * ymin) - exp(-K * ymax)) #average pressure
+        ρ0 = (rho0 * T0 * R_mass / g) * (exp(-K * ymin) - exp(-K * ymax)) #average density
+
+        volume = []
+        for par in particles
+                vol = par.m / par.rho
+                push!(volume, vol)
+        end
+        V0 = Statistics.mean(volume) #average volume
+
+        #α = 5e-3 #viscosity parameter
+        #β = 2 * p0 / ρ0
+        ζ = α * sqrt(β / V0)
+
+        return β, ζ, V0
 end
 
 #=
@@ -134,17 +108,17 @@ Move and accelerate
 =#
 
 function packing_accelerate!(p::AbstractParticle)
-    if p.type==FLUID
-	    p.u += 0.5*dt*p.Du
-    end
+        if p.type == FLUID
+                p.v += 0.5 * dt * p.Dv
+        end
 end
 
 function packing_move!(p::AbstractParticle)
-	p.Du = VEC0
-    p.gGamma=VEC0
-	if p.type == FLUID
-		p.x += dt*p.u
-	end
+        p.Dv = VEC0
+        p.gGamma = VEC0
+        if p.type == FLUID
+                p.x += dt * p.v
+        end
 
 end
 #=
@@ -152,16 +126,15 @@ Hard reset of velocities and accelerations, makes sure the initial state is prop
 =#
 
 function reset!(p::AbstractParticle)
-    p.Du=VEC0
-    p.u=VEC0
+        p.Dv = VEC0
+        p.v = VEC0
 end
 
 #= 
 Simple stopping criterion, combining absolute and relative tolerance
 =#
 
-function stopping_criterion(abs_tol::Float64,rel_tol::Float64,init_gGamma::Vector{Any})
-    return 2*abs_tol+rel_tol*(LinearAlgebra.norm(init_gGamma))
+function stopping_criterion(abs_tol::Float64, rel_tol::Float64, init_gGamma::Vector{Any})
+        return 2 * abs_tol + rel_tol * (LinearAlgebra.norm(init_gGamma))
 end
 
-end
--- mtn-wvs/testing/new_witch.jl
+++ mtn-wvs/testing/new_witch.jl
@@ -5,7 +5,6 @@
 
  all thermodynamic processes are adiabatic
 """
-
 module AdiabaticStaticWitch
 export main
 
@@ -13,8 +12,6 @@ using Printf
 using SmoothedParticles
 using DataFrames
 using Plots
-include("../../examples/utils/atmo_packing.jl")
-using .atmo_packing
 
 const folder_name = "../results/adiabatic_static_witch"
 const export_vars = (:v, :rho, :P, :θ, :T, :type)
@@ -26,17 +23,26 @@ const export_vars = (:v, :rho, :P, :θ, :T, :type)
 # geometry parameters
 const dom_height = 26e3        # height of the domain
 const dom_length = 400e3       # length of the domain
-const dr = dom_height / 50     # average particle distance
-const h0 = 1.8 * dr            # smoothing length
+const dr = dom_height / 75     # average particle distance
 const bc_width = 6 * dr        # boundary width
 const hₘ = 0.0                 # Witch of Agnesi: mountain height
 const a = 0.0                  # Witch of Agnesi: mountain width
 
+# smoothing paramaters
+const η = 1.8 # prefactor for a average number of neighbours
+const h0 = η * dr            # smoothing length
+
+
 # physical parameters
 const rho0 = 1.393             # reference density
-const mu = 1.0                 # dynamic viscosity
+const m0 = rho0 * dr * dr
 const c = sqrt(65e3 * (7 / 5) / rho0) # speed of sound
-const nu = 0.1 * h0 * c
+
+# artifical parameters
+const ν = 0.1 * h0 * c        # pressure stabilization
+const ε = 0.01
+const α = 0.1                # usually α = 0.05 - 0.2
+const β = 2 * α               # usually β = 2 α 
 
 # meteorological parameters
 const N = sqrt(0.0196)         # Brunt–Väisälä frequency
@@ -55,7 +61,7 @@ const T0 = 250.0               # initial temperature
 
 # temporal parameters
 const dt = 0.01 * h0 / c       # time step
-const t_end = 5.0              # end of simulation
+const t_end = 10.0              # end of simulation
 const dt_frame = t_end / 100   # frame interval
 
 # particle types
@@ -67,43 +73,49 @@ const MOUNTAIN = 2.0
 const rho_floor = 1e-6
 const P_floor = 1e-10
 
+include("../../examples/utils/new_packing.jl")
+
 # ==============
 # Particle type
 # ==============
 
 mutable struct Particle <: AbstractParticle
         h::Float64        # smoothing length
-        Dh::Float64       # rate of smoothing length
         x::RealVector     # position
         m::Float64        # mass
         v::RealVector     # velocity
         Dv::RealVector    # acceleration
+	ρ̄
+	ρ′
         rho::Float64      # density
-        Drho::Float64     # rate of density
         P::Float64        # kernel-averaged pressure (for output)
+	P̄
+	P′
         θ::Float64        # potential temperature
         T::Float64        # temperature
+	T̄
+	T′
         gGamma::RealVector
         type::Float64     # particle type
         A::Float64        # Hopkins entropy variable
-
+	
         function Particle(x::RealVector, v::RealVector, type::Float64)
                 obj = new(
-                        h0, 0.0,           # h, Dh
+                        h0,                # h, 
                         x, 0.0,            # x, m
                         v, VEC0,           # u, Dv
-                        0.0, 0.0,          # rho, Drho
+                        0.0,               # rho
                         0.0, 0.0,          # P, θ, 
                         0.0,               # T
                         VEC0,              # gGamma
                         type,              # type
-                        0.0,               # A, P
+                        0.0,               # A
                 )
 
                 # initial hydrostatic isothermal state 
                 obj.T = T0
                 obj.rho = rho0 * exp(-obj.x[2] * g / (R_mass * obj.T))
-                obj.m = obj.rho * dr^2
+                obj.m = obj.rho * dr * dr#m0
                 obj.P = R_mass * obj.T * obj.rho
                 obj.θ = obj.T * (((T0 * R_gas * rho0) / obj.P)^(2))^(1 / 7)
                 obj.A = obj.P / obj.rho^γ
@@ -116,7 +128,8 @@ end
 # ==============
 
 function make_system()
-        grid = Grid(dr, :hexagonal)
+        #grid = Grid(dr, :exp; K=g / (R_mass * T0))
+        grid = Grid(dr, :hexagonal; K=1.0)
         domain = Rectangle(-dom_length / 2.0, 0.0, dom_length / 2.0, dom_height)
         fence = BoundaryLayer(domain, grid, bc_width)
 
@@ -129,18 +142,22 @@ function make_system()
         generate_particles!(sys, grid, mountain, x -> Particle(x, VEC0, FLUID))
 
         create_cell_list!(sys)
-        improved_sys = sys#atmo_packing.packing(sys, 1e-10, 1e-10, 100)
-        create_cell_list!(improved_sys)
-        return improved_sys
+        packing!(sys)
+        create_cell_list!(sys)
+        return sys
 end
 
 # ==============
 # Thermodynamics helper: A and P
 # ==============
 
-@inbounds function compute_pressure_bar!(p::Particle, q::Particle, r::Float64)
+@inbounds function reset_pressure_bar!(p::Particle)
         if p.type == FLUID
                 p.P = 0.0
+        end
+end
+@inbounds function compute_pressure_bar!(p::Particle, q::Particle, r::Float64)
+        if p.type == FLUID
                 ker = wendland2(0.5 * (p.h + q.h), r)
                 p.P += q.m * q.A^(1 / γ) * ker
         end
@@ -173,17 +190,22 @@ end
 # ==============
 
 @inbounds function compute_density!(p::Particle, q::Particle, r::Float64)
-        if p.type == FLUID 
+        if p.type == FLUID
+                p.rho += q.m * wendland2(p.h, r)
+        end
+end
+
+@inbounds function reset_density!(p::Particle)
+        if p.type == FLUID
                 p.rho = 0.0
-                p.rho += q.m * wendland(p.h, r)
         end
 end
 
 @inbounds function update_smoothing!(p::Particle)
         if p.type == FLUID
-                p.h += dt * p.Dh
+                rho = max(p.rho, rho_floor)
+                p.h = η * sqrt(p.m / rho)
         end
-        p.Dh = 0.0
 end
 
 
@@ -202,31 +224,35 @@ end
 
 @inbounds function balance_of_momentum!(p::Particle, q::Particle, r::Float64)
         x_pq = p.x - q.x
+        v_pq = p.v - q.v
+        dot_product = SmoothedParticles.dot(x_pq, v_pq)
 
         if p.type == FLUID
-                # Hopkins pressure–entropy prefactor (Aᵢ Aⱼ)^{1/γ}
                 prefac = q.m * (p.A * q.A)^(1 / γ)
-
-                # P̄ exponents P̄^{1 - 2/γ}
                 expfac = 1.0 - 2.0 / γ
-
-                # use reduced derivative kernels with each particle's h
                 ker_i = rDwendland2(p.h, r)
                 ker_j = rDwendland2(q.h, r)
 
                 # pairwise conservative force
-                p.Dv += -prefac * (p.P^expfac * ker_i + q.P^expfac * ker_j) * x_pq
+                pP = max(p.P, P_floor)
+                qP = max(q.P, P_floor)
+                p.Dv += -prefac * (pP^expfac * ker_i + qP^expfac * ker_j) * x_pq
+                if dot_product < 0.0
+                        h_ij = 0.5 * (p.h + q.h)
+                        ker_ij = rDwendland2(h_ij, r)
+                        prho = max(p.rho, rho_floor)
+                        qrho = max(q.rho, rho_floor)
+                        c_i = sqrt(γ * p.P / prho)
+                        c_j = sqrt(γ * q.P / qrho)
+                        c_ij = 0.5 * (c_i + c_j)
+                        ρ_ij = 0.5 * (prho + qrho)
+                        μ_ij = (h_ij * dot_product) / (r * r + ε * h_ij * h_ij)
+                        π_ij = (-α * c_ij * μ_ij + β * μ_ij * μ_ij) / ρ_ij
 
-
-                # viscous term (kept from your original)
-                ρp = max(p.rho, rho_floor)
-                ρq = max(q.rho, rho_floor)
-                #ker_visc = (q.m / ρq) * rDwendland2(0.5 * (p.h + q.h), r)
-                #p.Dv += ρp * 8.0 * ker_visc * mu / (ρp * ρq) *
-                dot(p.v - q.v, x_pq) / (r * r + 0.0025 * (p.h + q.h)^2) * x_pq
+                        # artificial viscous force
+                        p.Dv += -q.m * π_ij * ker_ij * x_pq
+                end
         end
-
-
 end
 
 # ==============
@@ -234,7 +260,6 @@ end
 # ==============
 
 function move!(p::Particle)
-        p.Dv = VEC0
         if p.type == FLUID
                 p.x += dt * p.v
         end
@@ -244,6 +269,7 @@ function accelerate!(p::Particle)
         if p.type == FLUID
                 p.v += 0.5 * dt * (p.Dv - g * VECY - damping_structure(p.x[2], zₜ, zᵦ, γᵣ) * VECY)
         end
+        p.Dv = VEC0
 end
 
 # ==============
@@ -256,12 +282,14 @@ function verlet_step!(sys::ParticleSystem{Particle})
         apply!(sys, move!)
         create_cell_list!(sys)
 
-	# compute density and smoothing length
-	apply!(sys, compute_density!)
-	apply!(sys, update_smoothing!)
-	create_cell_list!(sys)
-	
+        # compute density and smoothing length
+        apply!(sys, reset_density!)
+        apply!(sys, compute_density!)
+        apply!(sys, update_smoothing!)
+        create_cell_list!(sys)
+
         # pressure–entropy: build P̄ from A
+        apply!(sys, reset_pressure_bar!)
         apply!(sys, compute_pressure_bar!)
         apply!(sys, finalize_pressure_bar!)
 
@@ -281,14 +309,14 @@ end
 function avg_velocity(sys::ParticleSystem)::Float64
         v = 0.0
         for p in sys.particles
-                v += norm(p.v)
+                v += SmoothedParticles.norm(p.v)
         end
         v = v / length(sys.particles)
         return v
 end
 
 function max_velocity(sys::ParticleSystem)::Float64
-        v = maximum(norm(p.v) for p in sys.particles)
+        v = maximum(SmoothedParticles.norm(p.v) for p in sys.particles)
         return v
 end
 
@@ -307,7 +335,6 @@ function main()
 
         @show T0
         @show rho0
-        @show mu
         @show c
         println("---------------------------")
 
